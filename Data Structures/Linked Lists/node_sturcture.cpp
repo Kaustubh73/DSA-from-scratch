d76d164cdce214ab7766f3247f341cb5f5d11a46
Kaustubhg73 <kaustubhg73@gmail.com>
2023-06-18 00:14:49 +0530

    Linked lists added

--- Data Structures/Linked Lists/node_sturcture.cpp
+++ Data Structures/Linked Lists/node_sturcture.cpp
@@ -33,30 +33,21 @@ node * insertEnd(node * head, int x)
 node * insertPos(node * head, int pos, int x)
 {
     node * curr = head;
+    node * temp = new node(x);
     if (pos < 1) 
     {
         cout << "Invalid position!" << endl;
         return head;
     }
-    else {
-        while (pos--)
+    else
+    {
+        int count = 1;
+        while (count != pos)
         {
-            if (pos == 0)
-            {
-                node * temp = new node(x);
-                temp->next = curr->next->next;
-                curr->next = temp;
-            }
-            else
-            {
-                if (curr->next == NULL)
-                {
-                    cout << "Invalid position!" << endl;
-                    return head;
-                }
-                curr = curr->next;
-            }
+            curr = curr->next;
         }
+        temp->next = curr->next;
+        curr->next = temp;
     }
     return head;
 }
@@ -66,6 +57,57 @@ void deleteNode(node * temp)
     temp->next = temp->next->next;
 }
 
+node * addTwoNumbers(node * A, node * B)
+{
+    if (A == NULL) return B;
+    if (B == NULL) return A;
+
+    stack<int> s1, s2;
+    while (A != NULL)
+    {
+        s1.push(A->data);
+        A = A->next;
+    }
+
+    while (B != NULL)
+    {
+        s2.push(B->data);
+        B = B->next;
+    }
+    int carry = 0;
+    node * result = NULL;
+
+    while (s1.empty() == false || s2.empty() == false) {
+        int a = 0, b = 0;
+        if (!s1.empty()) {
+            a = s1.top();
+            s1.pop();
+        }
+        if (!s2.empty())
+        {
+            b = s2.top();
+            s2.pop();
+        }
+        int total = a + b + carry;
+        carry = total/ 10;
+        node * temp = new node(total % 10);
+        if (result == NULL){
+            result = temp;
+        }
+        else 
+        {
+            temp->next = result;
+            result = temp;
+        }
+    }
+    if (carry != 0)
+    {
+        node * temp = new node(carry);
+        temp->next = result;
+        result = temp;
+    }
+    return result;
+}
 node * getIntersectionNode(node * headA, node * headB)
 {
     node * pA = headA;
@@ -98,9 +140,17 @@ int main(int argc, char * argv[])
     node *head = new node(3);
     head = insertBegin(head, 4);
     head = insertBegin(head, 2);
-    head = insertBegin(head, 43);  
-    head = insertBegin(head, 34);
-    head = insertPos(head, 2, 5);
+    head = insertBegin(head, 4);  
+    head = insertPos(head, 1, 5);
+    node * headA = new node(5);
+    headA = insertBegin(headA, 3);
+    headA = insertBegin(headA, 2);
+    headA = insertBegin(headA, 9);
+    headA = insertBegin(headA, 8);
+
+    node * result = addTwoNumbers(head, headA);
     printList(head);
+    printList(headA);
+    printList(result);
     return 0;
 }
